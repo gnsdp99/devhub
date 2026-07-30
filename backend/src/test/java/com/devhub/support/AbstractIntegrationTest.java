40b134e53ed4d89f0efabeab5403ad74550b4ac9
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2026-07-30 14:41:30 +0900

    refactor: 통합 테스트의 JdbcClient 접근을 공용 상위 클래스로 이동

--- backend/src/test/java/com/devhub/support/AbstractIntegrationTest.java
+++ backend/src/test/java/com/devhub/support/AbstractIntegrationTest.java
@@ -1,9 +1,18 @@
 package com.devhub.support;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
 @SpringBootTest
 @Import(ContainerConfig.class)
 public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected JdbcClient jdbcClient;
+
+    protected long count(String sql) {
+        return jdbcClient.sql(sql).query(Long.class).single();
+    }
 }
