0ded7e718fdf7a08059de418848305b7240f6b01
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2026-07-31 19:29:26 +0900

    refactor: API 예외를 support 공통 타입으로 정리 (#13)

--- backend/src/main/java/com/devhub/article/InvalidCursorException.java
+++ backend/src/main/java/com/devhub/article/InvalidCursorException.java
@@ -1,8 +1,11 @@
 package com.devhub.article;
 
-public class InvalidCursorException extends RuntimeException {
+import com.devhub.support.ApiException;
+import org.springframework.http.HttpStatus;
+
+public class InvalidCursorException extends ApiException {
 
     public InvalidCursorException(String cursor, Throwable cause) {
-        super("커서를 해석할 수 없습니다: " + cursor, cause);
+        super(HttpStatus.BAD_REQUEST, "커서를 해석할 수 없습니다: " + cursor, cause);
     }
 }
